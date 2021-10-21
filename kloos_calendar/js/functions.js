6b932d066fab0701514406c8cc9b5af6efb396c4
Juliano Meinen <julianomeinen.souza@gmail.com>
2021-10-21 17:09:47 -0300

    [FEAT] Task, action and template creation. [FEAT] Localization US and BR files update.

--- kloos_calendar/js/functions.js
+++ kloos_calendar/js/functions.js
@@ -1,1 +1,16 @@
+if (window.rcmail) {
+    rcmail.addEventListener('beforeswitch-task', function(prop) {
+        // catch clicks to calendar task button
+        console.log('prop',prop);
+        if (prop == 'calendar') {
+            if (rcmail.task == 'calendar'){  // we're already there
+                return false;
+            }
+            var url = rcmail.url('calendar.show_calendar');
+            rcmail.redirect(url, false);
+            return true;
+        }
+    });
+}
+
 console.log('Kloos Calendar is active');
--- kloos_calendar/kloos_calendar.php
+++ kloos_calendar/kloos_calendar.php
@@ -11,24 +11,66 @@
  */
 class kloos_calendar extends rcube_plugin
 {
-    
-    function init()
+
+    private $rc = null;
+
+    public function init()
     {
+        // Set the instances
+        $this->rc = rcmail::get_instance();
+
+        // Set localization
         $this->add_texts('localization', true);
+
+        // Incude JS and CSS files
         $this->include_stylesheet("css/style.css");
         $this->include_script('js/functions.js');
+
+        // register task
+        $this->register_task('calendar');
+        // register actions
+        $this->register_action('index', array($this, 'show_calendar'));
+
+        // Add the hooks
         $this->add_hook('startup', array($this, 'add_calendar_button_to_taskbar'));
     }
 
-    function add_calendar_button_to_taskbar($args)
+    public function add_calendar_button_to_taskbar($args)
     {
-
         $this->add_button(array(
-            'command'    => 'mail',
+            'command'    => 'calendar',
             'class'      => 'button_kloos_calendar',
             'innerclass' => 'inner',
             'label'      => 'kloos_calendar.button_label',
             'type'       => 'link',
         ), 'taskbar');
+    }
+
+    public function show_calendar()
+    {
+        $this->register_handler('plugin.calendar_html', array($this, 'get_calendar_index_html'));
+        $this->rc->output->set_pagetitle($this->gettext('kloos_calendar.page_title'));
+        //$rcmail->output->send('plugin');
+        $this->rc->output->send('kloos_calendar.calendar');
+    }
+
+    public function get_calendar_index_html(): string
+    {
+        $content = '';
+        $arrWeekDays = $this->get_week_days();
+        
+        $content .= implode(', ', $arrWeekDays);
+
+        return $content;
+    }
+
+    private function get_week_days(bool $abreviation = false): array
+    {
+        $arrWeekDays = [];
+
+        for ($i = 1; $i <= 7; $i++) {
+            $arrWeekDays[] = $abreviation === true ? mb_substr($this->gettext('kloos_calendar.weekday_' . $i), 0, 3) : $this->gettext('kloos_calendar.weekday_' . $i);
+        }
+        return $arrWeekDays;
     }
 }
